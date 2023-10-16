6bc1a6f37b5fb9e7daddb1ca785db021feb64359
chiara1911 <chiaracesari89@gmail.com>
2023-10-16 16:16:28 +0200

    add scaffolding

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// MILESTONE 1 - CREAZIONE DI UNA LISTA DELLA SPESA
+// Chiediamo (prompt) all'utente i prodotti da acquistare e mettiamoli da qualche parte (dove?)
+// Continuiamo a chiedere prodotti fino a quando l'utente non inserisce "stop"
